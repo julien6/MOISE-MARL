4807705eee0b92c029746316d2d5a385f3b5a155
Julien <julien.soule@lcis.grenoble-inp.fr>
2025-03-19 18:30:30 +0100

    improved oa settings

--- marllib_moise_marl/test_scenarios/clean.sh
+++ marllib_moise_marl/test_scenarios/clean.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 rm -rf ./exp_results/mappo_mlp_warehouse_management
 cp -r ./exp_results/mappo_mlp_warehouse_management_copy ./exp_results/mappo_mlp_warehouse_management
+
+
+rm -rf ./exp_results/mappo_mlp_asymmetric_advantages
+cp -r ./exp_results/mappo_mlp_asymmetric_advantages_copy ./exp_results/mappo_mlp_asymmetric_advantages
+
+rm -rf ./exp_results/mappo_mlp_simple_world_comm
+cp -r ./exp_results/mappo_mlp_simple_world_comm_copy ./exp_results/mappo_mlp_simple_world_comm
